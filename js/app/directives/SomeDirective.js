397f5e111ea4a22ca60a2552afded4d5e889a02f
lshapz <laurashapiro01@gmail.com>
2017-04-29 15:35:25 -0400

    Done.

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -5,9 +5,18 @@ function SomeDirective() {
 				'Replace this text!',
 			'</div>'
 		].join(''),
-		link: function (scope, elem, attrs) {
+		// link: function ($scope, $elem, $attrs) {
 
-		}
+		// },
+		compile: function ($element, $attrs) {
+       pre: function (scope, element, attrs) {
+          element = 'not this'
+      },
+      post: function(scope, element, attrs) {
+                console.log('post-link');
+      }
+    
+
 	}
 }
 
